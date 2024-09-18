6d051d842cd594b972847af16d6a2b5e5c0f4ff5
victor santos <victorvhjcsoares@gmail.com>
2024-09-18 13:58:45 -0300

    Update firestore.service.ts

--- src/app/firestore.service.ts
+++ src/app/firestore.service.ts
@@ -19,10 +19,10 @@ export class FirestoreService {
     return this.firestore.collection(collectionName).add(data);
   }
 
-  // Atualizar um documento na coleção 'users'
-async updateDocument(docId: string, data: any, p0: { fullname: string; }): Promise<void> {
-  return this.firestore.collection('users').doc(docId).update(data);
-}
+  // Atualizar um documento em uma coleção
+  async updateDocument(collection: string, docId: string, data: any): Promise<void> {
+    return this.firestore.collection(collection).doc(docId).update(data);
+  }
 
   // Excluir um documento de uma coleção
   async deleteDocument(collection: string, documentId: string): Promise<void> {
